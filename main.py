9e929a1d78b322c8335b3affdf2f8b83ed574629
Syuutowassyoi <syuuto5107@gmail.com>
2025-04-17 18:24:12 +0900

    update

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # または ["https://syuutowassyoi.github.io"]
     allow_methods=["*"],
     allow_headers=["*"],
 )
